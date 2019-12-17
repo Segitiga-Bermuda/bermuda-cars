5b6a0e1edb691dea6b0284f1aa5b86e790764fb6
GodsEnWrath <fastyazid@gmail.com>
2019-12-17 20:08:28 +0700

    add CRUD sales & graph

--- routes/executives/index.js
+++ routes/executives/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router(),
 
 router.post('/log-in', executivesRouter.logIn)
 router.put('/update', executivesRouter.updateOne)
+router.put('/avatar', executivesRouter.updateAvatar)
 
 
 module.exports = router
