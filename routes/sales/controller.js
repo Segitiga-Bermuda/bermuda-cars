24ea83bc59dea4dbc5436beb4abd497f4795bf89
A-Naive-Dreamer <dayakarcher@hotmail.com>
2019-12-18 09:23:24 +0700

    Send revenue data

--- routes/sales/controller.js
+++ routes/sales/controller.js
@@ -26,20 +26,20 @@ module.exports = {
   getYears: (req, res) => {
     db.sequelize
       .query(
-          "SELECT DISTINCT year from Sales;",
-          {
-            type: Sequelize.QueryTypes.SELECT
-          }
-        )
-        .then(result => {
-          res.status(200).send({
-            message: "Get all years.",
-            data: result
-          })
+        "SELECT DISTINCT year from Sales;",
+        {
+          type: Sequelize.QueryTypes.SELECT
+        }
+      )
+      .then(result => {
+        res.status(200).send({
+          message: "Get all years.",
+          data: result
         })
-        .catch(error => {
-          console.log(error)
-        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
   },
   getMonths: (req, res) => {
     db.sequelize
@@ -48,12 +48,12 @@ module.exports = {
         {
           type: Sequelize.QueryTypes.SELECT
         })
-        .then(async result => {
-          res.status(200).send({
-            message: "Get all months.",
-            data: result
-          })
+      .then(async result => {
+        res.status(200).send({
+          message: "Get all months.",
+          data: result
         })
+      })
   },
   getRevenueReports: async (req, res) => {
     let join = [];
@@ -65,28 +65,27 @@ module.exports = {
           {
             type: Sequelize.QueryTypes.SELECT
           })
-          .then(async result => {
-            await result.map(element => {
-              db.sequelize
-                .query(
-                  "SELECT SUM(Inventories.profit * Sales.sold) as profitTotal, SUM(Inventories.totalCost * Sales.unsold) as totalLoss  FROM Sales JOIN Inventories ON Sales.itemId = Inventories.id WHERE Sales.month='" + element.month + "' AND year=" + req.params.year + ";",
-                 { type: Sequelize.QueryTypes.SELECT }
-                )
-                .then(result2 => {
-                  console.log(result2)
-                })
-                .catch(error => {
-                  console.log(error)
+        .then(async result => {
+          await result.map(element => {
+            db.sequelize
+              .query(
+                "SELECT SUM(Inventories.profit * Sales.sold) as profitTotal, SUM(Inventories.totalCost * Sales.unsold) as totalLoss  FROM Sales JOIN Inventories ON Sales.itemId = Inventories.id WHERE Sales.month='" + element.month + "' AND year=" + req.params.year + ";",
+                { type: Sequelize.QueryTypes.SELECT }
+              )
+              .then(result2 => {
+                res.status(200).send({
+                  message: "Get all datas.",
+                  data: result2
                 });
-            })
-            res.status(200).send({
-              message: "Get all datas.",
-              data: ''
-            });
+              })
+              .catch(error => {
+                console.log(error)
+              });
           })
-          .catch(error => {
-            console.log(error)
-          });
+        })
+        .catch(error => {
+          console.log(error)
+        });
     } catch (error) {
       console.log(error);
     }
@@ -95,7 +94,7 @@ module.exports = {
     db.sequelize
       .query(
         "SELECT Inventories.item, Inventories.color, Sales.sold, Sales.unsold FROM Sales JOIN Inventories ON Sales.itemId = Inventories.id WHERE Sales.year=" + req.params.year + " AND Sales.month='" + req.params.month + "';",
-        { 
+        {
           type: Sequelize.QueryTypes.SELECT
         }
       )
@@ -106,123 +105,123 @@ module.exports = {
         })
       })
   },
-  addOne: async (req, res) => { 
+  addOne: async (req, res) => {
     try {
-      if(
+      if (
         !(
           req.user.role === 'Admin' ||
           req.user.role === 'Executive' ||
           req.user.role === 'Employer'
-        ) 
-    ){
-      res.send({
-        message: 'ordinary user cant edit this data'
-      })
-      return null
-    }
+        )
+      ) {
+        res.send({
+          message: 'ordinary user cant edit this data'
+        })
+        return null
+      }
 
-     await Sales
-     .create({
-        itemId: parseInt(req.params.itemId),
-        month: req.body.month,
-        year: parseInt(req.body.year),
-        sold: parseInt(req.body.sold),
-        unsold: parseInt(req.body.unsold),
-      })
-      .then(result => {
-        Sales
+      await Sales
+        .create({
+          itemId: parseInt(req.params.itemId),
+          month: req.body.month,
+          year: parseInt(req.body.year),
+          sold: parseInt(req.body.sold),
+          unsold: parseInt(req.body.unsold),
+        })
+        .then(result => {
+          Sales
             .findAll({})
             .then(result2 => {
-                res.send({
-                    message: 'Data is successfully added.',
-                    data: result2
-                })
+              res.send({
+                message: 'Data is successfully added.',
+                data: result2
+              })
             })
-    })
+        })
     } catch (error) {
       console.log(error);
     }
   },
 
 
-deleteOne: async (req, res) => {
+  deleteOne: async (req, res) => {
 
-  try {
-    if( !(
-      req.user.role === 'Admin' ||
-      req.user.role === 'Executive' )
-    ){
-    res.send({
-      message: 'ordinary user cant edit this data'
-    })
-    return null
-  }
-    await Sales
-    .destroy({
-      where: {
-        id: parseInt(req.params.id),
-      }
-    })  .then(result => {
-      Sales
-          .findAll({})
-          .then(result2 => {
-              res.send({
-                  message: 'Data is successfully deleted.',
-                  data: result2
-              })
-          })
-  })
-  } catch (error) {
-    console.log(error);
-  }
-},
-
-
-
-updateOne: async (req, res) => {
-
-  try {
-    if(
-      !(
-      req.user.role === 'Admin' ||
-      req.user.role === 'Executive' 
-      ) 
-  ){
-    res.send({
-      message: 'ordinary user cant edit this data'
-    })
-    return null
-  }
-        await Sales
-        .update(
-            {
-              month: req.body.month,
-              year: parseInt(req.body.year),
-              sold: parseInt(req.body.sold),
-              unsold: parseInt(req.body.unsold),
-            },
-            {
-                where: {
-                id: parseInt(req.params.id)
-            
-            }
+    try {
+      if (!(
+        req.user.role === 'Admin' ||
+        req.user.role === 'Executive')
+      ) {
+        res.send({
+          message: 'ordinary user cant edit this data'
         })
+        return null
+      }
+      await Sales
+        .destroy({
+          where: {
+            id: parseInt(req.params.id),
+          }
+        }).then(result => {
+          Sales
+            .findAll({})
+            .then(result2 => {
+              res.send({
+                message: 'Data is successfully deleted.',
+                data: result2
+              })
+            })
+        })
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+
+
+  updateOne: async (req, res) => {
+
+    try {
+      if (
+        !(
+          req.user.role === 'Admin' ||
+          req.user.role === 'Executive'
+        )
+      ) {
+        res.send({
+          message: 'ordinary user cant edit this data'
+        })
+        return null
+      }
+      await Sales
+        .update(
+          {
+            month: req.body.month,
+            year: parseInt(req.body.year),
+            sold: parseInt(req.body.sold),
+            unsold: parseInt(req.body.unsold),
+          },
+          {
+            where: {
+              id: parseInt(req.params.id)
+
+            }
+          })
         .then(result => {
           Sales
-              .findAll({})
-              .then(result2 => {
-                  res.send({
-                      message: 'Data is successfully updated.',
-                      data: result2
-                  })
+            .findAll({})
+            .then(result2 => {
+              res.send({
+                message: 'Data is successfully updated.',
+                data: result2
               })
-      })
+            })
+        })
     } catch (error) {
-        console.log(error);
-        
-        
+      console.log(error);
+
+
     }
-},
+  },
 
 
 
