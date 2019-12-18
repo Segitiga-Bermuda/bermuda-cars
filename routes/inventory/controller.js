4078a03205b2a3e748d6e231b7e60f80773644ab
A-Naive-Dreamer <dayakarcher@hotmail.com>
2019-12-18 15:40:24 +0700

    Fix some bugs.

--- routes/inventory/controller.js
+++ routes/inventory/controller.js
@@ -5,139 +5,138 @@ module.exports = {
   getAll: async (req, res) => {
     try {
       await Inventories
-      .findAll({})
-      .then(result => {
-        res.status(200).send({
-          message: "Get all datas.",
-          data: result
+        .findAll({})
+        .then(result => {
+          res.status(200).send({
+            message: "Get all datas.",
+            data: result
+          });
         });
-      });
-    } catch (error) {
-        console.log(error);
-    }
-  },
-
-
-  addOne: async (req, res) => { 
-
-    try {
-      if(
-        !(
-          req.user.role === 'Admin' ||
-          req.user.role === 'Executive' ||
-          req.user.role === 'Employer'
-        ) 
-    ){
-      res.send({
-        message: 'ordinary user cant edit this data'
-      })
-      return null
-    }
-
-     await Inventories
-     .create({
-        id: req.params.id,
-        item: req.body.item,
-        price: req.body.price,
-        materialCost: req.body.materialCost,
-        laborCost: req.body.laborCost,
-        overheadCost: req.body.overheadCost,
-        color: req.body.color,
-        profit: parseInt(req.body.price) - parseInt(req.body.materialCost) - parseInt(req.body.laborCost) - parseInt(req.body.overheadCost),
-        totalCost: parseInt(req.body.materialCost) + parseInt(req.body.laborCost) + parseInt(req.body.overheadCost)
-      })
-      .then(result => {
-        Inventories
-            .findAll({})
-            .then(result2 => {
-                res.send({
-                    message: 'Data is successfully added.',
-                    data: result2
-                })
-            })
-    })
     } catch (error) {
       console.log(error);
     }
   },
 
-updateOne: async (req, res) => {
 
-  try {
-    if(
-      !(
-      req.user.role === 'Admin' ||
-      req.user.role === 'Executive'
-      ) 
-  ){
-    res.send({
-      message: 'ordinary user cant edit this data'
-    })
-    return null
-  }
-        await Inventories
-        .update(
-            {
-                item: req.body.item,
-                price: req.body.price,
-                materialCost: req.body.materialCost,
-                laborCost: req.body.laborCost,
-                overheadCost: req.body.overheadCost
-            },
-            {
-                where: {
-                id: parseInt(req.params.id)
-            
-            }
+  addOne: async (req, res) => {
+
+    try {
+      if (
+        !(
+          req.user.role === 'Admin' ||
+          req.user.role === 'Executive' ||
+          req.user.role === 'Employer'
+        )
+      ) {
+        res.send({
+          message: 'ordinary user cant edit this data'
+        })
+        return null
+      }
+
+      await Inventories
+        .create({
+          item: req.body.item,
+          price: req.body.price,
+          materialCost: req.body.materialCost,
+          laborCost: req.body.laborCost,
+          overheadCost: req.body.overheadCost,
+          color: req.body.color,
+          profit: parseInt(req.body.price) - parseInt(req.body.materialCost) - parseInt(req.body.laborCost) - parseInt(req.body.overheadCost),
+          totalCost: parseInt(req.body.materialCost) + parseInt(req.body.laborCost) + parseInt(req.body.overheadCost)
         })
         .then(result => {
           Inventories
-              .findAll({})
-              .then(result2 => {
-                  res.send({
-                      message: 'Data is successfully updated.',
-                      data: result2
-                  })
-              })
-      })
-    } catch (error) {
-        console.log(error);
-        
-        
-    }
-},
-deleteOne: async (req, res) => {
-
-  try {
-    if( !(
-      req.user.role === 'Admin' ||
-      req.user.role === 'Executive' 
-    )
-    ){
-    res.send({
-      message: 'ordinary user cant edit this data'
-    })
-    return null
-  }
-    await Sales
-    .destroy({
-      where: {
-        id: parseInt(req.params.id),
-      }
-    })  .then(result => {
-      Inventories
-          .findAll({})
-          .then(result2 => {
+            .findAll({})
+            .then(result2 => {
               res.send({
-                  message: 'Data is successfully deleted.',
-                  data: result2
+                message: 'Data is successfully added.',
+                data: result2
               })
+            })
+        })
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  updateOne: async (req, res) => {
+
+    try {
+      if (
+        !(
+          req.user.role === 'Admin' ||
+          req.user.role === 'Executive'
+        )
+      ) {
+        res.send({
+          message: 'ordinary user cant edit this data'
+        })
+        return null
+      }
+      await Inventories
+        .update(
+          {
+            item: req.body.item,
+            price: req.body.price,
+            materialCost: req.body.materialCost,
+            laborCost: req.body.laborCost,
+            overheadCost: req.body.overheadCost
+          },
+          {
+            where: {
+              id: parseInt(req.params.id)
+
+            }
           })
-  })
-  } catch (error) {
-    console.log(error);
-  }
-},
+        .then(result => {
+          Inventories
+            .findAll({})
+            .then(result2 => {
+              res.send({
+                message: 'Data is successfully updated.',
+                data: result2
+              })
+            })
+        })
+    } catch (error) {
+      console.log(error);
+
+
+    }
+  },
+  deleteOne: async (req, res) => {
+
+    try {
+      if (!(
+        req.user.role === 'Admin' ||
+        req.user.role === 'Executive'
+      )
+      ) {
+        res.send({
+          message: 'ordinary user cant edit this data'
+        })
+        return null
+      }
+      await Sales
+        .destroy({
+          where: {
+            id: parseInt(req.params.id),
+          }
+        }).then(result => {
+          Inventories
+            .findAll({})
+            .then(result2 => {
+              res.send({
+                message: 'Data is successfully deleted.',
+                data: result2
+              })
+            })
+        })
+    } catch (error) {
+      console.log(error);
+    }
+  },
 
 
 };
